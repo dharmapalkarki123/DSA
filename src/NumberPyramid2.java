fe6042e6ba755ae72ac3c4fe47855f5a74b5c914
unknown <you@example.com>
2025-03-21 23:28:26 +0545

    commit

--- src/NumberPyramid2.java
+++ src/NumberPyramid2.java
@@ -5,7 +5,7 @@ public class NumberPyramid2 {
 		int n=5;
 		for(int i=1;i<=n;i++) {
 			
-			for(int j=1;j<=n-1;i++) {
+			for(int j=1;j<=n-i;j++) {
 				System.out.print(" ");
 			}
 			
@@ -20,3 +20,26 @@ public class NumberPyramid2 {
 	}
 
 }
+
+
+//public class NumberPyramid2 {
+//
+//    public static void main(String[] args) {
+//        int n = 5;
+//        
+//        for (int i = 1; i <= n; i++) {
+//
+//            // Print leading spaces
+//            for (int j = 1; j <= n - i; j++) {  // Fixed: Changed "i" to "j" and corrected condition
+//                System.out.print(" ");
+//            }
+//
+//            // Print numbers
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(i + " ");
+//            }
+//
+//            System.out.println();
+//        }
+//    }
+//}
