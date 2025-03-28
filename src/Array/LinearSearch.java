3feaffd15a2e4086cda2321dbc28a34c18c44833
dharmapalkarki123 <dharmapalkarki@gmail.com>
2025-03-28 20:45:24 +0545

    commit

---
+++ src/Array/LinearSearch.java
@@ -0,0 +1,35 @@
+package Array;
+
+import java.util.Scanner;
+
+
+public class LinearSearch {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner sc	=new Scanner(System.in);
+		int num =sc.nextInt();
+		
+		int array[]=new int[num];
+		
+		for(int i=0;i<array.length;i++) {
+			
+		array[i]=sc.nextInt();	
+			
+		}
+		
+		int x=5;
+				for(int i=0;i<array.length;i++) {
+					if(array[i]==x) {
+						System.out.println("Number found in index of" +i);
+					}
+				}
+		for(int i=0;i<array.length;i++) {
+			System.out.println(array[i]);
+		}
+	}
+	
+	
+	
+}
