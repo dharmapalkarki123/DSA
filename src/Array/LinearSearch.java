6f57a5effa69c2e646988507f5cdfe3516fcc908
dharmapalkarki123 <dharmapalkarki@gmail.com>
2025-03-31 20:19:12 +0545

    commit

--- src/Array/LinearSearch.java
+++ src/Array/LinearSearch.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class LinearSearch {
 
 	
-	public static void main(String[] args) {
+	public static void main(SSTRING[] args) {
 		
 		Scanner sc	=new Scanner(System.in);
 		int num =sc.nextInt();
---
+++ src/Array/SSTRING.java
@@ -0,0 +1,43 @@
+package Array;
+
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.jar.Attributes.Name;
+
+public class SSTRING {
+
+	public static void main(java.lang.String[] args) {
+//		Scanner sc =new Scanner(System.in);
+//		System.out.println("Enter Your Name ");
+//		String s1=sc.nextLine();
+//		
+//		System.out.println("Your Name is " +s1);
+		//concatinate
+		String s1="Tony";
+		String s2="Stark";
+		String s3=s1+" " +s2;
+		
+		System.out.println(s3);
+		System.out.println(s3.length());
+		
+//		for(int i=0;i<s3.length();i++) {
+//			
+//			System.out.println(s3.charAt(i));
+//			
+//		}
+		
+		if(s1.compareTo(s2)==0) {
+			System.out.println("String are equal");
+		}else {
+			System.out.println("String are not equal");
+		}
+		
+		
+		String sentence="My name is Tom Hardy";
+		String name= sentence.substring(11);
+		System.out.println(name);
+		
+
+	}
+
+}
