db03ffe66335af783cb6c780a92798952d00ddf1
dharmapalkarki123 <dharmapalkarki@gmail.com>
2025-04-29 19:07:49 +0545

    commit

---
+++ src/Array/ProductExceptSelf.java
@@ -0,0 +1,34 @@
+package Array;
+
+public class ProductExceptSelf {
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] output = new int[n];
+        
+        output[0] = 1;
+        
+        // Left products
+        for (int i = 1; i < n; i++) {
+            output[i] = output[i-1] * nums[i-1];
+        }
+        
+        // Right products
+        int right = 1;
+        for (int i = n-1; i >= 0; i--) {
+            output[i] = output[i] * right;
+            right *= nums[i];
+        }
+        
+        return output;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        int[] result = productExceptSelf(nums);
+        
+        for (int val : result) {
+            System.out.print(val + " ");
+        }
+    }
+}
+
