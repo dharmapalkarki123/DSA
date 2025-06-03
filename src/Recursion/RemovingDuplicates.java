146a54ba4d9da43b12c146a3c255839e1e1136fb
dharmapalkarki123 <dharmapalkarki@gmail.com>
2025-06-03 12:19:15 +0545

    commit

--- src/Recursion/RemovingDuplicates.java
+++ src/Recursion/RemovingDuplicates.java
@@ -1,9 +1,46 @@
 package Recursion;
 
-public class RemovingDuplicates {
 
+
+public class RemovingDuplicates {
+	public static boolean[] map = new boolean[26]; 
+
+	
+	public static void removeDupli(String str,int idx, String newString) {
+		
+		
+		if(idx==str.length()) {
+			
+			System.out.println(newString);
+			
+			return;
+			
+			
+		}
+		
+		char currChar=str.charAt(idx);
+		if(map[currChar-'a']==true) {
+			
+			removeDupli(str, idx+1, newString);
+			
+		}else {
+			newString+=currChar;
+			
+			map[currChar-'a']=true;
+			removeDupli(str, idx+1, newString);
+			
+		}
+		
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		String str ="aabbcabddce";
+		removeDupli(str, 0, "");
+
+		
 
 	}
 
