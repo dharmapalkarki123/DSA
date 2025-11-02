471d7c13d76184f5933e2eae8b818fbdffacf575
dharmapalkarki123 <dharmapalkarki@gmail.com>
2025-11-02 20:23:41 +0545

    commit

--- src/Conditional.java
+++ src/Conditional.java
@@ -80,18 +80,93 @@ public class Conditional {
 //		}
 		
 		
-		for(int i=4; i>=1;i--) {
-			
-			for(int j=1; j<=i; j++)
-			{
+//		for(int i=4; i>=1;i--) {
+//			
+//			for(int j=1; j<=i; j++)
+//			{
+//				
+//				System.out.print("*");
+//			
+//			}
+//			
+//			System.out.println(" ");
+//			
+//		}
+		
+		
+//		for(int i=1;i<=4;i++) {
+//			for(int j=1; j<=4-i; j++) {
+//				System.out.print(" ");
+//			}
+//			
+//			for(int j=1; j<=i;j++) {
+//				System.out.print("*");
+//			}
+//			
+//			System.out.println();
+//		}
+//		
+		
+		for(int i=4;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
 				
-				System.out.print("*");
-			
+				System.out.print(j +" ");
+				
+				
+				
 			}
+			System.out.println();
 			
-			System.out.println(" ");
 			
 		}
+		
+		System.out.println();
+		
+		
+
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
+				
+				System.out.print(j +" ");
+				
+				
+				
+			}
+			System.out.println();
+			
+			
+		}
+		
+		System.out.println();
+		
+		
+		
+		int num=1;
+		
+		
+		
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
+				
+				System.out.print(num);
+				
+				num++;
+				
+			}
+			System.out.println();
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
