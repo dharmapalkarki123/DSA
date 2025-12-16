7c5023640a60ad2b185f6c35cc11e8c35293ffdb
dharmapalkarki123 <dharmapalkarki@gmail.com>
2025-12-16 20:37:04 +0545

    commit

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -19,24 +19,18 @@ public class BubbleSort {
 		
 		
 	  for(int i=0; i<array.length-1;i++) {
+	  for(int j=0;j<array.length-i-1;j++) {
 		  
-		  int smallestIndex=i;
-		  
-		  for(int j=i+1;j<array.length;j++) {
+		  if(array[j]>array[j+1]) {
 			  
-		if (array[smallestIndex]>array[j]) {
-			
-			smallestIndex=j;
-			
-		}
-			  
-			
+			   int temp=array[j];
+			  array[j]=array[j+1];
+			  array[j+1]=temp;
 			  
 		  }
 		  
-		  int temp=array[smallestIndex];
-			array[smallestIndex]=array[i];
-			array[i]=temp;
+	  }
+	
 	  }
 	  
 	  printArryay(array);
