f9c9cf54ebb5a430a5d79bd08fdd62029ccaa5dd
dharmapalkarki123 <dharmapalkarki@gmail.com>
2025-12-20 21:06:29 +0545

    commit

--- src/Recursion/SumOfNaturalNum.java
+++ src/Recursion/SumOfNaturalNum.java
@@ -1,44 +1,83 @@
 package Recursion;
 
+import java.security.PublicKey;
 import java.util.Scanner;
 
 public class SumOfNaturalNum {
 
-	public static void printSum(int n,int i,int sum) {
+//	public static void printSum(int n,int i,int sum) {
+//		
+//		if(n==i) {
+//			
+//			sum=sum+i;
+//			
+//			System.out.println(sum);
+//			
+//			return;
+//			
+//		}
+//		
+//		sum=sum+i;
+//		
+//		printSum(n, i+1, sum);
+//		
+//		System.out.println(i);
+//		
+//		
+//		
+//		
+//	
+//		
+//		
+//		
+//		
+//	}
+//	
+//	
+//	public static void main(String[] args) {
+//		
+//		
+//		printSum(5, 1, 0);
+//	
+//
+//	}
+	
+	
+	public static void printSum(int n,int i, int sum) {
 		
-		if(n==i) {
+		if(i==n) {
 			
-			sum=sum+i;
-			
+//			sum += i;
 			System.out.println(sum);
 			
-			return;
 			
+			return;
 		}
 		
-		sum=sum+i;
 		
+		
+		sum += i;
 		printSum(n, i+1, sum);
 		
-		System.out.println(i);
-		
-		
-		
-		
-	
-		
-		
-		
-		
 	}
+	
+	
+	
 	
 	
 	public static void main(String[] args) {
 		
+		printSum(5,1,0);
 		
-		printSum(5, 1, 0);
-	
-
+		
+		
 	}
+	
+	
+	
+	
+	
+	
+	
 
 }
