163bd9a0f2a621ef6b18a3c9c1c1b13512aba322
dharmapalkarki123 <dharmapalkarki@gmail.com>
2025-12-26 19:53:50 +0545

    commit

--- src/Recursion/PowerCal.java
+++ src/Recursion/PowerCal.java
@@ -3,32 +3,29 @@ package Recursion;
 public class PowerCal {
 	public static int powerCal(int x, int n) {
 		
-		if(n==0) {
-			return 1;
+		
+		if(x==0) {
+			return 0;
 		}
-			if (x==0) {
-				
-				return 0;
-				
-			}	//if n is even
-				if(n%2==0) {
-					
-					return powerCal(x,n/2)*powerCal(x, n/2);
-					
-				}else { //n is odd
-					
-					return powerCal(x,n/2)*powerCal(x, n/2)*x;
-					
-				}
-				
-				
+		if (n==0) {
+			return 1;
+			
+		}
+		
+		if(n%2==0) {
+			
+			return powerCal(x, n/2)*powerCal(x, n/2);
+			
+		}else  
+			
+			return powerCal(x, n/2)*powerCal(x, n/2)*x;
 				
 				
 			}
 
 			public static void main(String[] args) {
 				
-				int x=4 , n=2;
+				int x=4 , n=5;
 				
 				int ans=powerCal(x, n);
 				System.out.println(ans);
