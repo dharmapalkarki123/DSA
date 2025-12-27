79a719df0d1877f46d3dc3292c7944e4c0283394
dharmapalkarki123 <dharmapalkarki@gmail.com>
2025-12-27 19:29:13 +0545

    commit

--- src/Recursion/TowerOfHanoi.java
+++ src/Recursion/TowerOfHanoi.java
@@ -6,24 +6,31 @@ public class TowerOfHanoi {
 
     // Recursive function to solve Tower of Hanoi
     public static void solveHanoi(int n, String src, String helper, String des) {
-        if (n == 1) {
-            System.out.println("Move disk 1 from " + src + " to " + des);
-            return;
-        }
+    	
+    	if(n==0) {
+    		
+    		System.out.println("transfer disk"+n+"from" +src+ "to" +des);
+    		return;
 
-        // Move top n-1 disks from source to helper
-        solveHanoi(n - 1, src, des, helper);
-
-        // Move the nth disk from source to destination
-        System.out.println("Move disk " + n + " from " + src + " to " + des);
-
-        // Move the n-1 disks from helper to destination
-        solveHanoi(n - 1, helper, src, des);
+    		
+    	}
+    	
+    	
+    	solveHanoi(n-1, src, des, helper);
+    	
+    	System.out.println("transfer disk"+n+"from" +src+ "to" +des);
+    	
+    	solveHanoi(n-1, helper, src, des);
+    	
+    	
+   
     }
 
     public static void main(String[] args) {
-        int numberOfDisks = 10;
-        solveHanoi(numberOfDisks, "S", "H", "D");
+    	
+    	
+    	int n=2;
+      solveHanoi(n, "S", "H", "D");
     }
 }
 
