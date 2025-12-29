6a7fed6d05e3befb0b45bed5d36cb00a2b0a2647
dharmapalkarki123 <dharmapalkarki@gmail.com>
2025-12-29 23:22:59 +0545

    commit

---
+++ src/Recursion/Occerance2.java
@@ -0,0 +1,47 @@
+package Recursion;
+
+
+
+public class Occerance2 {
+	
+	public static int first =-1;
+	public static int last= -1;
+	
+	public static void occerance(String str, int idx, char element) {
+	
+		
+		if(idx==str.length()) {
+			
+			System.out.println(first);
+			System.out.println(last);
+			return;
+		}
+		
+		
+		char currentElemnt= str.charAt(idx);
+		
+		if(currentElemnt==element) {
+			 
+			if(first==-1) {
+				
+				first=idx;
+			}else {
+				last=idx;
+			}
+			
+		}
+		
+		occerance(str, idx+1, element);
+		
+		
+	}
+
+	public static void main(String[] args) {
+		String str="abaacda";
+		
+			
+
+		occerance(str, 0, 'a');
+	}
+
+}
