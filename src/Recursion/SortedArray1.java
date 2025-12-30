ee17160b0db6b68af48caee309419af1f41f4566
dharmapalkarki123 <dharmapalkarki@gmail.com>
2025-12-30 20:26:49 +0545

    commit

---
+++ src/Recursion/SortedArray1.java
@@ -0,0 +1,41 @@
+package Recursion;
+
+public class SortedArray1 {
+	
+	
+	public static boolean isSorted(int arr[], int idx) {
+		
+		if(idx==arr.length-1) {
+			
+			
+			return true;
+		}
+		
+		
+		if(arr[idx]<arr[idx+1]) {
+			
+			return isSorted(arr, idx+1);
+			
+		}else {
+			return false;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	public static void main(String[] args) {
+		int[] arr= {1,5,3,4};
+		
+		System.out.println(isSorted(arr,0));
+		
+		
+
+	}
+
+}
