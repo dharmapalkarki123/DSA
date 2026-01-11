fce3cf536b1c55652b53bb809342b264905ff346
dharmapalkarki123 <dharmapalkarki@gmail.com>
2026-01-11 22:36:00 +0545

    commit

---
+++ src/Recursion/ArrayList.java
@@ -0,0 +1,39 @@
+package Recursion;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayList {
+	
+	  static void subtractAndPrint(int[] array) {
+
+	        // subtract 1 from each element (not below 0)
+	        for (int i = 0; i < array.length; i++) {
+	            if (array[i] > 0) {
+	                array[i]--;
+	            }
+	        }
+
+	        // print current state
+	        System.out.println(Arrays.toString(array));
+
+	        // base case: all elements are zero
+	        for (int i = 0; i < array.length; i++) {
+	            if (array[i] != 0) {
+	                subtractAndPrint(array); // recurse
+	                return;
+	            }
+	        }
+	            }
+	        
+
+
+	public static void main(String[] args) {
+		int[] array = {1, 2, 3};
+		subtractAndPrint(array);
+		
+		
+
+	}
+
+}
