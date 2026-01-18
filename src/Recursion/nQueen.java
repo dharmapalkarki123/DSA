4f899960cde4ec4b2769233737971099d87ba2ab
dharmapalkarki123 <dharmapalkarki@gmail.com>
2026-01-18 17:07:16 +0545

    commit

--- src/Recursion/nQueen.java
+++ src/Recursion/nQueen.java
@@ -25,7 +25,67 @@ public class nQueen {
     		
     		
     	}
-    	
+    	solveNqueen(board,0);
 
     }
+
+	private static void solveNqueen(char[][] board, int row) {
+		int n=board.length;
+		
+		if(row==n) {
+			
+			for(int i=0; i<n; i++) {
+	    		
+	    		for(int j=0;j<n;j++) {
+	    			
+	    			System.out.println(board[i][j]);
+	    		
+	    			
+	    		}
+	    		
+	    		System.out.println();
+			
+		}
+			return;
+		}
+		for(int j=0;j<n;j++) {
+			
+			if(isSafe(board,row,j)) {
+				
+				board[row][j]='Q';
+				solveNqueen(board, row+1);
+				board[row][j]='.'; //backtacking
+				
+				
+			}
+			
+			
+		}
+		
+	}
+
+	private static boolean isSafe(char[][] board, int row, int col) {
+		
+		//for row
+		for(int j=0;j<board.length;j++) {
+			
+			if(board[row][j]=='Q') {
+				return false;
+			}
+			
+		}
+		
+		
+		for(int i=0;i<board.length;i++) {
+			
+			if(board[i][col]=='Q') {
+				return false;
+			}
+			
+		}
+		
+		
+		
+		return true;
+	}
 }
