796764729ef035e3366ad0e330a2e343cacc1130
dharmapalkarki123 <dharmapalkarki@gmail.com>
2026-01-24 20:54:52 +0545

    commit

--- src/OOPS.java
+++ src/OOPS.java
@@ -1,24 +1,76 @@
 
-class Pen{
+//class Pen{
+//	
+//	String color;
+//	String type;
+//	
+//	
+//	public void write() {
+//		
+//		System.out.println("Write something");
+//			
+//		
+//		
+//	}
+//	
+//	public void printColor() {
+//		
+//		
+//		System.out.println(this.color);
+//		System.out.println(this.type);
+//	}
+//	
+//	
+//}
+
+
+
+class Student{
 	
-	String color;
-	String type;
+	String name;
+	int age ;
 	
 	
-	public void write() {
+	
+	public void printInfo() {
 		
-		System.out.println("Write something");
-			
+		System.out.println(this.name);
+		System.out.println(this.age);
 		
 		
 	}
 	
-	public void printColor() {
+	
+	//constructor non parameterized
+	Student(){
+		
+		System.out.println("Constructor is called");
 		
 		
-		System.out.println(this.color);
-		System.out.println(this.type);
+		
 	}
+	
+	
+	
+	
+	//parameterized constructor
+	Student(String name, int age){
+		
+		this.name=name;
+		this.age=age;
+		
+		
+		
+	}
+	
+	
+	//copy constructor
+	Student(Student s3){
+		this.name=s3.name;
+		this.age=s3.age;
+		
+	}
+	
 	
 	
 }
@@ -27,7 +79,9 @@ class Pen{
 public class OOPS {
 
 	public static void main(String[] args) {
-	  Pen pen1=new Pen(); //object creation of class
+	 
+		/*
+		Pen pen1=new Pen(); //object creation of class
 	  
 	  pen1.color="blue";
 	  pen1.type="ballpen";
@@ -41,6 +95,27 @@ public class OOPS {
 	  pen2.type="cello";
 	  
 	  pen2.printColor();
+	  
+	  
+	  */
+	  
+		
+		//non parameterized constructor exmaple object
+		Student s1=new Student();
+		s1.name="aman";
+		s1.age=24;
+		s1.printInfo();
+	  
+	  
+		
+		//parameterized constructor related object
+	  Student s2=new Student("aman", 24);
+	  s2.printInfo();
+	  
+	  
+	  Student s3=new Student(s2);
+	  s3.printInfo();
+	  
 	  
 	  
 
