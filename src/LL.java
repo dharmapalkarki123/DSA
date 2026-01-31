dfa9cacca88ff10d5e06e78c4fffa44b6c55d4d1
dharmapalkarki123 <dharmapalkarki@gmail.com>
2026-01-31 20:05:06 +0545

    commit

--- src/LL.java
+++ src/LL.java
@@ -2,6 +2,11 @@
 public class LL {
 
 	Node head;
+	int size;
+
+	LL() {
+		this.size = 0;
+	}
 
 	class Node {
 
@@ -12,6 +17,8 @@ public class LL {
 
 			this.data = data;
 			this.next = null;
+
+			size++;
 
 		}
 
@@ -55,99 +62,88 @@ public class LL {
 		currNode.next = newNode;
 
 	}
-	
-	
-	
-	
-	//delete first
-	public  void deleteFirst() {
-		
-		if(head==null) {
+
+	// delete first
+	public void deleteFirst() {
+
+		if (head == null) {
 			System.out.println("List is empty");
-			
-		}
-		head=head.next;
-		
-		
-	}
-	
-	
-	//delete last
-	public void deleteLast() {
-		
-		if(head==null) {
-			
-			System.out.println("List is empty");
-			
-			
-		}
-		
-		if(head.next==null) {
-			head=null;
-		}
-		
-		
-		
-		Node secondLastNode=head;
-		Node lasNode=head.next;
-		
-		while(lasNode.next!=null) {
-			
-			lasNode=lasNode.next;
-			secondLastNode=secondLastNode.next;
-			
+
 		}
 
-		secondLastNode.next=null;
-		
-		
+		size--;
+		head = head.next;
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	// delete last
+	public void deleteLast() {
+
+		if (head == null) {
+
+			System.out.println("List is empty");
+
+		}
+		size--;
+
+		if (head.next == null) {
+			head = null;
+		}
+
+		Node secondLastNode = head;
+		Node lasNode = head.next;
+
+		while (lasNode.next != null) {
+
+			lasNode = lasNode.next;
+			secondLastNode = secondLastNode.next;
+
+		}
+
+		secondLastNode.next = null;
+
+	}
+
 	public void printList() {
-		if(head==null) {
+		if (head == null) {
 			System.out.println("List is Empty");
 			return;
 		}
-		
+
 		Node currNode = head;
 
-		while (currNode.next!= null) {
-			
-			System.out.print(currNode.data+"->");
+		while (currNode.next != null) {
+
+			System.out.print(currNode.data + "->");
 
 			currNode = currNode.next;
 
 		}
-		
+
 		System.out.print("Null");
-	
-		
-		
+
+	}
+
+	public int getSize() {
+		return size;
 	}
 
 	public static void main(String[] args) {
 
 		LL list = new LL();
+
 		list.addFirst("is");
 		list.addFirst("this");
-		
+
 		list.addLast("a");
 		list.addLast("ball");
-		list.deleteFirst();
-		list.deleteLast();
+		 list.deleteFirst();
+		 list.deleteLast();
 		list.printList();
+		
+		System.out.println();
+
+		System.out.println(list.getSize());
 
 	}
 
