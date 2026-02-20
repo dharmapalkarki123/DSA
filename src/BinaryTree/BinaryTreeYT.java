af8f0a131f2bb082b76805290d574203975d33b1
dharmapalkarki123 <dharmapalkarki@gmail.com>
2026-02-20 20:49:14 +0545

    commit

--- src/BinaryTree/BinaryTreeYT.java
+++ src/BinaryTree/BinaryTreeYT.java
@@ -1,75 +1,95 @@
 package BinaryTree;
 
 public class BinaryTreeYT {
-	
-	
-	static class Node{
+
+	static class Node {
 		int data;
 		Node leftNode;
 		Node rightNode;
-		
-		
-		
-		Node(int data){
-			
-			this.data=data;
-			this.leftNode=null;
-			this.rightNode=null;
-			
+
+		Node(int data) {
+
+			this.data = data;
+			this.leftNode = null;
+			this.rightNode = null;
+
 		}
-		
+
 	}
-	
-	
-	static class BinaryTree{
-		
-		static int idx=-1;
-		
+
+	static class BinaryTree {
+
+		static int idx = -1;
+
 		public static Node buildTree(int nodes[]) {
-			
+
 			idx++;
-			
-			if(nodes[idx]==-1) {
+
+			if (nodes[idx] == -1) {
 				return null;
 			}
-			
-			Node newNode=new Node(nodes[idx]);
-			newNode.leftNode=buildTree(nodes);
-			newNode.rightNode=buildTree(nodes);
-			
+
+			Node newNode = new Node(nodes[idx]);
+			newNode.leftNode = buildTree(nodes);
+			newNode.rightNode = buildTree(nodes);
+
 			return newNode;
-			
-			
+
 		}
-		
+
 	}
-	
-	
+
+	public static void preorder(Node rootNode) {
+
+		if (rootNode == null) {
+			return;
+		}
+
+		System.out.print(rootNode.data + " ");
+
+		preorder(rootNode.leftNode);
+		preorder(rootNode.rightNode);
+
+	}
+
+	public static void inorderTraversal(Node rootNode) {
+
+		if (rootNode == null) {
+
+			return;
+		}
+
+		inorderTraversal(rootNode.leftNode);
+		System.out.print(rootNode.data + " ");
+		inorderTraversal(rootNode.rightNode);
+
+	}
+
+	public static void postTraversal(Node rootNode) {
+
+		if (rootNode == null) {
+
+			return;
+		}
+
+		postTraversal(rootNode.leftNode);
+		postTraversal(rootNode.rightNode);
+		System.out.print(rootNode.data + " ");
+
+	}
 
 	public static void main(String[] args) {
-		int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+		int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
 
-		BinaryTree tree=new BinaryTree();
-	Node rootNode=	 tree.buildTree(nodes);
-		
-		
-		
-		System.out.println(rootNode.data);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		BinaryTree tree = new BinaryTree();
+		Node rootNode = tree.buildTree(nodes);
+
+		// System.out.println(rootNode.data);
+		// preorder(rootNode);
+
+		// inorderTraversal(rootNode);
+
+		postTraversal(rootNode);
+
 	}
 }
