1f48686a65c04b67ae55f2d7786a8b4d4e88202b
dharmapalkarki123 <dharmapalkarki@gmail.com>
2026-03-12 17:50:35 +0545

    commit

--- src/TwoDArray.java
+++ src/TwoDArray.java
@@ -2,52 +2,44 @@ import java.util.Scanner;
 
 public class TwoDArray {
 
-	public static void main(String[] args) {
-		
-		Scanner sc =new Scanner(System.in);
-		
-		int rows=sc.nextInt();
-		int cols=sc.nextInt();
-		
-		int[][] arrays=new int[rows][cols];
-		
-		for(int i=0;i<rows;i++)
-		{
-		  for(int j=0;j<cols;j++)
-		  {
-			  arrays[i][j]=sc.nextInt();
-			  
-			  }
-		}
-		
-		for(int i=0;i<rows;i++) {
-			
-			for(int j=0;j<cols;j++) {
-				
-				System.out.print(arrays[i][j] +" ");
-				
-			}
-			
-			System.out.println();
-			
-			
-			
-			
-		}
-		
-		int a=sc.nextInt();
-		int b=sc.nextInt();
-		
-		
-		int c=a+b;
-		
-		
-		System.out.println(c);
-		
-	
-	}
-	
-	
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
+        // Input rows and columns
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        // Create 2D array
+        int[][] arrays = new int[rows][cols];
+
+        // Input elements into the array
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                arrays[i][j] = sc.nextInt();
+            }
+        }
+
+        // Print the 2D array
+        System.out.println("Matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(arrays[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // Input two numbers
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // Add the numbers
+        int c = a + b;
+
+        // Print result
+        System.out.println("Sum: " + c);
+
+        // Close scanner
+        sc.close();
+    }
 }
