85ae82fa080d2c361b84fac0fc43326c2aaefb85
dharmapalkarki123 <dharmapalkarki@gmail.com>
2026-03-20 21:03:39 +0545

    commit

--- src/Hash/SubarraySumEqualsK.java
+++ src/Hash/SubarraySumEqualsK.java
@@ -4,33 +4,41 @@ import java.util.HashMap;
 
 public class SubarraySumEqualsK {
 
-    public static int subarraySum(int[] nums, int k) {
+    public static int subarraySum(int[] arr, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         
         map.put(0, 1); // Important: handles sum = k case
         
-        int count = 0;
-        int sum = 0;
-        
-        for (int num : nums) {
-            sum += num;
-            
-            // Check if (sum - k) exists
-            if (map.containsKey(sum - k)) {
-                count += map.get(sum - k);
-            }
-            
-            // Store current sum
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
-        }
-        
-        return count;
+      int ans=0;
+      int sum=0;
+      
+      for(int j=0;j<arr.length;j++) {
+    	  
+    	  sum +=arr[j];
+    	  
+    	  if(map.containsKey(sum-k)) {
+    		  
+    		  ans+=map.get(sum-k);
+    	  }
+    	  
+    	  if(map.containsKey(sum)) {
+    		  
+    		  map.put(sum, map.get(sum)+1);
+    		  
+    	  }else {
+    		  
+    		  map.put(sum, 1);
+    		  
+    	  }
+    	  
+      }
+      return ans;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 1, 1};
-        int k = 2;
+        int[] arr = {10, 2,-2,-20, 10};
+        int k = -10;
         
-        System.out.println("Number of subarrays: " + subarraySum(nums, k));
+        System.out.println("Number of subarrays: " + subarraySum(arr, k));
     }
 }
