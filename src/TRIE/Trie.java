428883da241d1be585621861d65de9dd0cdc222b
dharmapalkarki123 <dharmapalkarki@gmail.com>
2026-03-23 22:35:47 +0545

    commit

--- src/TRIE/Trie.java
+++ src/TRIE/Trie.java
@@ -1,89 +1,80 @@
 package TRIE;
 
-class TrieNode {
-    TrieNode[] children = new TrieNode[26];
-    boolean isEndOfWord;
 
-    // Constructor
-    TrieNode() {
-        isEndOfWord = false;
-        for (int i = 0; i < 26; i++) {
-            children[i] = null;
-        }
-    }
-}
 
 public class Trie {
 
-    TrieNode root;
-
-    // Constructor
-    Trie() {
-        root = new TrieNode();
-    }
-
-    // 1. Insert word
-    public void insert(String word) {
-        TrieNode current = root;
-
-        for (int i = 0; i < word.length(); i++) {
-            int index = word.charAt(i) - 'a';
-
-            if (current.children[index] == null) {
-                current.children[index] = new TrieNode();
-            }
-
-            current = current.children[index];
-        }
-
-        current.isEndOfWord = true;
-    }
-
-    // 2. Search word
-    public boolean search(String word) {
-        TrieNode current = root;
-
-        for (int i = 0; i < word.length(); i++) {
-            int index = word.charAt(i) - 'a';
-
-            if (current.children[index] == null) {
-                return false;
-            }
-
-            current = current.children[index];
-        }
-
-        return current.isEndOfWord;
-    }
-
-    // 3. StartsWith (prefix check)
-    public boolean startsWith(String prefix) {
-        TrieNode current = root;
-
-        for (int i = 0; i < prefix.length(); i++) {
-            int index = prefix.charAt(i) - 'a';
-
-            if (current.children[index] == null) {
-                return false;
-            }
-
-            current = current.children[index];
-        }
-
-        return true;
-    }
+	static class Node{
+		
+		Node[] children;
+		boolean endOfWord;
+		
+		public Node() {
+			
+			children=new Node[26];
+			
+			for(int i=0;i<26;i++) {
+				
+				children[i]=null;
+				
+			}
+			
+			endOfWord=false;
+			
+		}
+		
+		
+	}
+	
+	static Node rootNode=new Node();
+	
+  	
+	public static void insert(String word) {
+		
+		
+		for(int i=0;i<word.length();i++) {
+			
+			int idx=word.charAt(i)-'a';
+			
+			if(rootNode.children[idx]==null) {
+				
+				//add new node
+				rootNode.children[idx]=new Node();
+				
+			}
+			
+			if(i==word.length()-1) {
+				
+				rootNode.children[idx].endOfWord=true;
+				
+			}
+			
+		rootNode=	rootNode.children[idx];
+			
+			
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+  
+    
 
     // Main method to test
     public static void main(String[] args) {
-        Trie trie = new Trie();
-
-        trie.insert("apple");
-        trie.insert("app");
-
-        System.out.println(trie.search("apple"));   // true
-        System.out.println(trie.search("app"));     // true
-        System.out.println(trie.search("ap"));      // false
-
-        System.out.println(trie.startsWith("ap"));  // true
+    
+    	
+    String	words[]= {"the","a","there","their","any"};
+    
+    for(int i=0;i<words.length;i++) {
+    	
+    	insert(words[i]);
+    }
+    	
     }
 }
